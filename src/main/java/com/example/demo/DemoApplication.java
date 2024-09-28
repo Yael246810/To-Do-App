c3b15ca5bf6e47445b909b23e67849e1a5cf2b41
Yael246810 <yaelcohen691@gmail.com>
2024-09-28 21:53:27 +0300

    Update DemoApplication.java
    removed uneeded TODO

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 //TODO: to fix update customer (add another condition).
-//TODO:
 
 @SpringBootApplication
 public class DemoApplication {
