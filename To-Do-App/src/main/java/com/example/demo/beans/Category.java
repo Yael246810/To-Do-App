c1264ed169ee9a3d4febeeaef8bc0d62214d3818
Yael246810 <yaelcohen691@gmail.com>
2024-09-28 21:44:49 +0300

    Removed unneeded folder
    Removed unneeded folder

--- To-Do-App/src/main/java/com/example/demo/beans/Category.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.beans;
-
-public enum Category {
-    TAG,
-    LIST,
-    PROJECT
-}
--- To-Do-App/src/main/java/com/example/demo/utils/testUtils.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.utils;
-
-public class testUtils {
-    private static int count = 1;
-
-    public static void test(String title){
-        System.out.println(String.format("Test %03d - %s\n ,",count++,title));
-    }
-}
